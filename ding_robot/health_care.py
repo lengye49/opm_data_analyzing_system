0079ac51ed67d8fbb791b7ab05eebdf107dcdf0d
xuhaihua <lengye49@126.com>
2021-06-18 10:00:53 +0800

    health care

--- ding_robot/health_care.py
+++ ding_robot/health_care.py
@@ -5,11 +5,17 @@ import json
 import requests
 import time
 import threading
+import datetime
+import random
 
 
 def ding_talk(content=''):
     url = 'https://oapi.dingtalk.com/robot/send?access_token' \
           '=583a8ee9420b1d0fe980dc2133b04fb1c3955d6cfb61469b7d2f0cc40e02dd29 '
+
+    # url = 'https://oapi.dingtalk.com/robot/send?access_token' \
+    #       '=786b6e320aa4bc4889d5924c014b1b68d31f6d90e116d4597e347cbae655020c '
+
     content = '起来嗨:\n' + content
     headers = {
         "Content-Type": "application/json;charset=utf-8"
@@ -31,29 +37,25 @@ def ding_talk(content=''):
     print(result.text)
 
 
+def get_rand_player():
+    return players[random.randint(0,len(players))]
+
+
 def check_time():
-    # print('checking', time.localtime())
-    now_day = time.strftime('%Y%m%d', time.localtime())
-
-    # 节假日接口(工作日对应结果为 0, 休息日对应结果为 1, 节假日对应的结果为 2 )
-    server_url = "http://www.easybots.cn/api/holiday.php?d="
-    req = requests.get(server_url + now_day)
-    vop_data = json.loads(req.text)
-
     now_hour = time.localtime().tm_hour
 
     global last_hour
     if now_hour in [11, 15, 17] and last_hour != now_hour:
-        if int(vop_data[now_day]) == 0:
-            ding_talk('到点了！\n\n来呀，动起来！Come On Baby！')
-        elif int(vop_data[now_day]) == 1:
-            ding_talk('到点了！\n\n虽然是周末，也不要懈怠哦！动起来吧！')
-        elif int(vop_data[now_day]) == 2:
-            ding_talk('到点了！\n\n假期怎么能少了锻炼，快动起来吧！来一组！')
+        standard_day = datetime.date(2021, 6, 13)
+        curr_day = datetime.date.today()
+        w = (curr_day - standard_day).days % 7
+        s = '这一轮有请 -->' + get_rand_player() + '<-- 带头锻炼！\n\n'
+        if w in [1, 2, 3, 4, 5]:
+            ding_talk('到点了！\n\n来呀，动起来！Come On Baby！\n\n' + s)
         else:
-            print('connect error!')
+            ding_talk('到点了！\n\n虽然是周末，也不要懈怠哦！动起来吧！\n\n' + s)
+
     last_hour = now_hour
-    # print('last_hour', last_hour)
 
 
 def timer_task():
@@ -61,11 +63,12 @@ def timer_task():
     check_time()
 
     global timer
-    timer= threading.Timer(300, timer_task)
+    timer = threading.Timer(300, timer_task)
     timer.start()
 
 
 last_hour = 0
+players = ['苏成峰', '郑勇', '李涵笑', '范哲源', '王时雨', '陈乾', '王沛鸿', '徐海华']
 
 if __name__ == "__main__":
     timer = threading.Timer(2, timer_task)
